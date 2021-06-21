19c9b48b35908d66ec800e9834fdeb5b1b76c232
uchida-masanori <76242252+uchida-masanori@users.noreply.github.com>
2021-06-21 17:22:11 +0700

    functionOfTodo

--- src/app/todo/todo/todo.component.ts
+++ src/app/todo/todo/todo.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { tap } from 'rxjs/operators';
+import { AuthService } from 'src/app/services/auth.service';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
+  todos$ = this.todo.getTodos().pipe(tap((data) => console.log(data)));
+  user$ = this.auth.user$;
+  lists = ['todo', 'doing', 'done'];
+  form = this.fb.group({
+    name: ['', [Validators.required, Validators.maxLength(10)]],
+  });
+  constructor(
+    private todo: TodoService,
+    private auth: AuthService,
+    private fb: FormBuilder
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void { }
+  createTodo(uid: string) {
+    const name = this.form.controls.name.value;
+    this.todo.createTodo(name, uid);
+    this.form.reset();
+    this.form.controls.name.setErrors({ required: null });
   }
-
 }
