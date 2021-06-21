164c5f3b9cd84fc97b3ef1b6fc0a9fea0909e380
uchida-masanori <76242252+uchida-masanori@users.noreply.github.com>
2021-06-21 11:32:24 +0700

    functionOfLogin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'demo-app';
+  user$ = this.auth.user$;
+
+  constructor(private auth: AuthService) {}
+  login() {
+    this.auth.login();
+  }
+  logout() {
+    this.auth.logout();
+  }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,9 +6,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
-
-
 @NgModule({
   declarations: [],
   imports: [CommonModule],
